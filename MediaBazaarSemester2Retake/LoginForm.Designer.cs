3530972fee089a5db986acb784c0d60fecead75a
I493163@fhict.nl <I493163@fhict.nl>
2024-02-27 15:03:27 +0100

    FormLogin

---
+++ MediaBazaarSemester2Retake/LoginForm.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace MediaBazaarSemester2Retake
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.button4 = new System.Windows.Forms.Button();
+            this.BtnGo = new System.Windows.Forms.Button();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.btnCantSignIn = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CheckBoxStaySignedIn = new System.Windows.Forms.CheckBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtBoxPassword = new System.Windows.Forms.TextBox();
+            this.txtBoxUsername = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1552, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 37);
+            this.button4.TabIndex = 50;
+            this.button4.Text = "x";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // BtnGo
+            // 
+            this.BtnGo.CausesValidation = false;
+            this.BtnGo.Location = new System.Drawing.Point(108, 556);
+            this.BtnGo.Name = "BtnGo";
+            this.BtnGo.Size = new System.Drawing.Size(170, 123);
+            this.BtnGo.TabIndex = 49;
+            this.BtnGo.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreateAccount.Location = new System.Drawing.Point(81, 841);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(226, 57);
+            this.btnCreateAccount.TabIndex = 48;
+            this.btnCreateAccount.Text = "Create Account?";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            // 
+            // btnCantSignIn
+            // 
+            this.btnCantSignIn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCantSignIn.Location = new System.Drawing.Point(81, 779);
+            this.btnCantSignIn.Name = "btnCantSignIn";
+            this.btnCantSignIn.Size = new System.Drawing.Size(226, 55);
+            this.btnCantSignIn.TabIndex = 47;
+            this.btnCantSignIn.Text = "Cant sign in ?";
+            this.btnCantSignIn.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.White;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 11.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(86, 391);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 31);
+            this.label7.TabIndex = 46;
+            this.label7.Text = "Password";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(81, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 31);
+            this.label6.TabIndex = 45;
+            this.label6.Text = "Username";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label5.Location = new System.Drawing.Point(66, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(240, 48);
+            this.label5.TabIndex = 43;
+            this.label5.Text = "MediaBazaar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(324, 849);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 25);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "V 0.1";
+            // 
+            // CheckBoxStaySignedIn
+            // 
+            this.CheckBoxStaySignedIn.AutoSize = true;
+            this.CheckBoxStaySignedIn.Location = new System.Drawing.Point(121, 501);
+            this.CheckBoxStaySignedIn.Name = "CheckBoxStaySignedIn";
+            this.CheckBoxStaySignedIn.Size = new System.Drawing.Size(151, 29);
+            this.CheckBoxStaySignedIn.TabIndex = 41;
+            this.CheckBoxStaySignedIn.Text = "Stay Signed In";
+            this.CheckBoxStaySignedIn.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(84, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(151, 102);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 40;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(121, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 38);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Sign In";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(254, 424);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 38;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtBoxPassword
+            // 
+            this.txtBoxPassword.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtBoxPassword.Location = new System.Drawing.Point(81, 424);
+            this.txtBoxPassword.Multiline = true;
+            this.txtBoxPassword.Name = "txtBoxPassword";
+            this.txtBoxPassword.PasswordChar = '*';
+            this.txtBoxPassword.Size = new System.Drawing.Size(225, 51);
+            this.txtBoxPassword.TabIndex = 37;
+            // 
+            // txtBoxUsername
+            // 
+            this.txtBoxUsername.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtBoxUsername.Location = new System.Drawing.Point(81, 326);
+            this.txtBoxUsername.Multiline = true;
+            this.txtBoxUsername.Name = "txtBoxUsername";
+            this.txtBoxUsername.Size = new System.Drawing.Size(225, 51);
+            this.txtBoxUsername.TabIndex = 36;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(452, -1);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(1156, 920);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 44;
+            this.pictureBox4.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1606, 919);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.BtnGo);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Controls.Add(this.btnCantSignIn);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CheckBoxStaySignedIn);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtBoxPassword);
+            this.Controls.Add(this.txtBoxUsername);
+            this.Controls.Add(this.pictureBox4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button4;
+        private Button BtnGo;
+        private Button btnCreateAccount;
+        private Button btnCantSignIn;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private CheckBox CheckBoxStaySignedIn;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private TextBox txtBoxPassword;
+        private TextBox txtBoxUsername;
+        private PictureBox pictureBox4;
+    }
+}
