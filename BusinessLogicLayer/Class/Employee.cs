72a6ff0f35172d8c48dcb0395022f9742fe4cd28
Gavin <505876@student.fontys.nl>
2024-03-10 21:12:28 +0100

    Add random generate password

--- BusinessLogicLayer/Class/Employee.cs
+++ BusinessLogicLayer/Class/Employee.cs
@@ -1,4 +1,6 @@
-﻿namespace BusinessLogicLayer.Class
+﻿using System;
+
+namespace BusinessLogicLayer.Class
 {
     public class Employee
     {
@@ -20,6 +22,7 @@
         private string _emergencyRelation;
         private string _password;
         private bool _firstLogin;
+        private bool _active;
 
         public Employee(int employeID, string firstName, string lastName, string bsn, DateOnly dateOfBirth, int phoneNumber, string gender, string email, string city, string country, string street, int houseNumber, string postalCode, string emergencyContactName, int emergencyPhoneNumber, string emergencyRelation)
         {
@@ -39,9 +42,32 @@
             _emergencyContactName = emergencyContactName;
             _emergencyPhoneNumber = emergencyPhoneNumber;
             _emergencyRelation = emergencyRelation;
-            // password random?
-            _password = "test";
+            _password = GeneratePassword(8);
             _firstLogin = false;
+            _active = true;
+        }
+
+        public Employee(int employeID, string firstName, string lastName, string bsn, DateOnly dateOfBirth, int phoneNumber, string gender, string email, string city, string country, string street, int houseNumber, string postalCode, string emergencyContactName, int emergencyPhoneNumber, string emergencyRelation, string password, bool firstLogin, bool active)
+        {
+            _employeeID = employeID;
+            _firstName = firstName;
+            _lastName = lastName;
+            _bsn = bsn;
+            _dateOfBirth = dateOfBirth;
+            _phoneNumber = phoneNumber;
+            _gender = gender;
+            _email = email;
+            _city = city;
+            _country = country;
+            _street = street;
+            _houseNumber = houseNumber;
+            _postalCode = postalCode;
+            _emergencyContactName = emergencyContactName;
+            _emergencyPhoneNumber = emergencyPhoneNumber;
+            _emergencyRelation = emergencyRelation;
+            _password = password;
+            _firstLogin = firstLogin;
+            _active = active;
         }
 
         public string Name
@@ -66,6 +92,22 @@
             {
                 // nothing
             }
+        }
+
+        private string GeneratePassword(int length)
+        {
+            Random random = new Random();
+            string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+            char[] randomPassword = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                randomPassword[i] = characters[random.Next(characters.Length)];
+            }
+
+            string generatedPassword = new string(randomPassword);
+            return generatedPassword;
         }
 
         public override string ToString()
