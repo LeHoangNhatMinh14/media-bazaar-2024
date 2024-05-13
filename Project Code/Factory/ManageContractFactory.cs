83b6f689dd48a65563350845844ba4b80c3609e6
I493163@fhict.nl <I493163@fhict.nl>
2024-05-13 08:54:40 +0100

    test automated with preconfig steps, Forms update to display them based on rolew in the main form

--- Project Code/Factory/ManageContractFactory.cs
+++ Project Code/Factory/ManageContractFactory.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 
 namespace Factory
 {
-<<<<<<< HEAD
 	public class ManageContractFactory
 	{
 		public static ManageContract manageContract;
@@ -21,19 +20,5 @@ namespace Factory
 			return manageContract;
 		}
 	}
-=======
-    public class ManageContractFactory
-    {
-        public static ManageContract manageContract;
 
-        public static ManageContract Create()
-        {
-            if (manageContract == default)
-            {
-                manageContract = new (new ContractRepo());
-            }
-            return manageContract;
-        }
-    }
->>>>>>> db69de2e9d4107cdcc8b28012bc69f78f807ff03
 }
