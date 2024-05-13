6888c9baf8b116bb39e1bdc07dd405454a33cab9
José Gonçalves Pestana <i506501@FontysICT.nl>
2024-05-13 16:01:22 +0200

    AvailabilityWork

--- Project Code/BusinessLogicLayer/ManageClass/ManageContract.cs
+++ Project Code/BusinessLogicLayer/ManageClass/ManageContract.cs
@@ -1,42 +1,30 @@
 ﻿using BusinessLogicLayer.Class;
 using BusinessLogicLayer.Interface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BusinessLogicLayer.ManageClass
 {
-    public class ManageContract
-    {
-        private List<Contract> contracts;
-        private IContractsDAL contractsDAL;
+	public class ManageContract
+	{
+		private List<Contract> contracts;
+		private IContractsDAL contractsDAL;
 
-<<<<<<< HEAD
-        public ManageContract(IContractsDAL contractsDAL)
-        {
-            contracts = new List<Contract>();
-            this.contractsDAL = contractsDAL;
-=======
-        public ManageContract(IContractsDAL contract) 
-        {
-            contractsDAL = contract;
-            contracts = new List<Class.Contract>();
->>>>>>> db69de2e9d4107cdcc8b28012bc69f78f807ff03
-        }
+		public ManageContract(IContractsDAL contractsDAL)
+		{
+			contracts = new List<Contract>();
+			this.contractsDAL = contractsDAL;
+		}
 
-        public void AddContract(Contract contract)
-        {
-            contractsDAL.AddContract(contract);
-        }
-        
-        public Contract GetContract(int employeeID)
-        {
-            Contract contract = contractsDAL.GetContract(employeeID);
-            return contract;
-        }
+		public void AddContract(Contract contract)
+		{
+			contractsDAL.AddContract(contract);
+		}
+
+		public Contract GetContract(int employeeID)
+		{
+			Contract contract = contractsDAL.GetContract(employeeID);
+			return contract;
+		}
 
 
-    }
+	}
 }
---
+++ Project Code/WebApp/Pages/AvailabilityRequests.cshtml.cs
@@ -0,0 +1,17 @@
+using BusinessLogicLayer.Class;
+using BusinessLogicLayer.ManageClass;
+using Factory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace MediaBazaar_WebApp.Pages
+{
+    public class AvailabilityRequestModel : PageModel
+    {
+
+		public void OnGet()
+		{
+		}
+	}
+}
